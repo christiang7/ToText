2e34a648e3cba81f5cc6585b07c313539fe99b6d
christiang7 <98482045+christiang7@users.noreply.github.com>
2023-02-07 23:48:21 +0100

    changed zenity requst to one request

--- zim-template-program.sh
+++ zim-template-program.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-if zenity --question --text="Möchten Sie ein neues Programm schreiben?"
-then
 txtFile=$(echo "$1")
 folder=${txtFile%.*}
 mkdir -p "$folder"
@@ -9,46 +7,54 @@ filetxt=$(echo $2)
 filepath=$(echo "${filetxt%/*}" | sed "s,/home/christian,~,")
 wikipath=$(echo $filepath | sed "s,~/Gedankenspeicher/Gedankenspeicherwiki/,," | sed "s,/,:,g")
 FullFilename=$(basename $filetxt .txt)
-File=$(zenity --entry \
+File="Program"
+extens="sh"
+
+abfrage=$(zenity --forms \
        --width 500 \
-       --title "Type new filename" \
-       --text "Enter new filename" \
-       --entry-text "")
-extens=$(zenity --entry \
-       --width 500 \
-       --title "Type new filename" \
-       --text "Enter new filename" \
-       --entry-text "sh")
+       --title "New Program?" \
+       --text "Necessary Informations:" \
+       --add-entry "Filename" --add-entry "Extension Standard sh")
+
+if [ ! $? -eq 1 ];
+then
+
+if [[ ! "$abfrage" = "" ]];
+then
+	File=$(echo $abfrage | cut -s -d "|" -f 1)
+	extens=$(echo $abfrage | cut -s -d "|" -f 2)
+fi
 
 File=$(echo "$File" | sed 's/ /_/g' | sed 's/:/;/g'| sed -e "s/'/_/g" | sed 's/\"//g')
 
-source=$(zenity --entry \
-       --width 500 \
-       --title "Noch eine Quelle hinzufügen?" \
-       --text "Noch eine Quelle hinzufügen?" \
-       --entry-text "Christian Gößl")
-tags=$(zenity --entry \
-       --width 500 \
-       --title "Noch Schlagwörter hinzufügen?" \
-       --text "Noch Schlagwörter hinzufügen?" \
-       --entry-text "${3}@")
-additiontext=$(zenity --entry \
+source="Christian Gößl"
+tags=$(echo "$3")
+additiontext=$(echo "$4")
+
+abfrage=$(zenity --forms \
        --width 500 \
        --title "Noch etwas hinzufügen?" \
        --text "Noch etwas hinzufügen?" \
-       --entry-text "$4")
+       --add-entry "Quelle Standard: Christian Gößl" --add-entry "Schlagwörter" --add-entry "Weiteres")
+if [[ ! "$abfrage" = "" ]];
+then
+	source=$(echo $abfrage | cut -s -d "|" -f 1)
+	tags=$(echo $abfrage | cut -s -d "|" -f 2)
+	additiontext=$(echo $abfrage | cut -s -d "|" -f 3)
+fi
 
 echo "Content-Type: text/x-zim-wiki" > "${folder}"/"${File}".txt
 echo "Wiki-Format: zim 0.6" >> "${folder}"/"${File}".txt
 echo -e "===== ${File} =====" >> "${folder}"/"${File}".txt
-echo -e "Created $(date +"%A") $(date +[[Zettelkasten:%Y:%m:%d]])" >> "${folder}"/"${File}".txt
+echo -e "Created $(date +[[Zettelkasten:%Y:%m:%d]])" >> "${folder}"/"${File}".txt
 echo -e "Backlink [[$wikipath:$FullFilename]]" >> "${folder}"/"${File}".txt
 #"${filepath}.txt
 #echo -e "$([[Zettelkasten:%Y:%m:%d]])" >> "${folder}"/"${File}".txt
 echo -e "[[../]]" >> "${folder}"/"${File}".txt
-echo -e "[*] ${tags} ** ${File} ** >  2277-11-11" >> "${folder}"/"${File}".txt
+echo -e "[*] ${tags} ** ${File} ** ${source} >  2277-11-11" >> "${folder}"/"${File}".txt
 echo -e "\n${additiontext}" >> "${folder}"/"${File}".txt
-echo -e "\n''noweb.py -R${File}.${extens} ${File}.txt > ${File}.${extens} && chmod u+x ${File}.${extens} && ln -sf "${folder}"/${File}.${extens} ~/.local/bin/${File}.${extens} && echo 'fertig'''" >> "${folder}"/"${File}".txt
+echo -e "\n''noweb.py -R${File}.${extens} ${File}.txt > ${File}.${extens} && echo 'fertig'''" >> "${folder}"/"${File}".txt
+echo -e "\n\n''chmod u+x ${File}.${extens} && ln -sf "${folder}"/${File}.${extens} ~/.local/bin/${File}.${extens} && echo 'fertig'''" >> "${folder}"/"${File}".txt
 echo -e "\n{{{code: lang="sweave" linenumbers="True"" >> "${folder}"/"${File}".txt
 echo -e "<<${File}.${extens}>>=" >> "${folder}"/"${File}".txt
 echo -e "\n@" >> "${folder}"/"${File}".txt
