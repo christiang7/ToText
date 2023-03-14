0124c7d401cd2e2b58a265b4ec6f9e1ed53b0576
christiang7 <98482045+christiang7@users.noreply.github.com>
2023-03-14 02:26:53 +0100

    zim-script, ttnm and Download Music adjustments
    changing folder for moving files in ttnm
    correct Download-Music and extract-music paths
    changing zim-script in showing output in yad notifcation

--- zim-script.sh
+++ zim-script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-output=$(konsole --hold -e sh -c "$1 ; exec bash")
-#output=$(xterm -e "$1; bash" &)
-#output=$(konsole -e "$1")
-#echo $output
-#zenity --info --text "Prozess ausgeführt" --title "Programm ausgeführt"
-#zenity --info --text "Prozess fertiggestellt, hier ist der output: $output" --title "Prozess Fertiggestellt"
+output="$(eval $@ 2>&1)"
+yad --timeout=15 --timeout-indicator=top --form --separator="|" --item-separator="," \
+ 	 --field="Output Script:":TXT \
+ 	 "$output" --mouse
