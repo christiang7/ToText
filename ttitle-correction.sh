62f5b44cfaaee365f5da6f47edf385c5cf002050
christiang7 <98482045+christiang7@users.noreply.github.com>
2023-04-19 00:58:56 +0200

    eliminate comments

--- ttitle-correction.sh
+++ ttitle-correction.sh
@@ -2,8 +2,7 @@
 
 File=$(basename "$1")
 echo "$File"
-#check=true
-#checkk=true
+
 line=$(sed -n '/\[\*\]/{=;}' $File )
 line=${line:0:1}
 if [[ $line > 5 ]]
@@ -15,15 +14,3 @@ else
     sed -i "$line s/\[\*\]/===== $File =====\\n\[\*\]/" "$File"
 fi
 
-#echo $check
-#echo $checkk
-#if [[ $check == true ]]
-#then
-    #sed -i "4 s/\[\*\]/===== $File =====\\n\[\*\]/" "$File" && exit 1
-    #sed -i "4 s/\[ \]/===== $File =====\\n\[ \]/" "$File" && exit 1
-#fi
-#if [[ $checkk == true ]]
-#then
-    #sed -i "5 s/\[\*\]/===== $File =====\\n\[\*\]/" "$File"
-    #sed -i "5 s/\[ \]/===== $File =====\\n\[ \]/" "$File"
-#fi
