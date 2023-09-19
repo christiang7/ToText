27a4bebc9aaae4886e3c04b10be2961a7063f05c
christiang7 <98482045+christiang7@users.noreply.github.com>
2023-09-19 18:12:00 +0200

    small corections to zim-draft-page.sh

--- zim-draft-page.sh
+++ zim-draft-page.sh
@@ -5,6 +5,8 @@ echo "Content-Type: text/x-zim-wiki" > "${filename}".md
 echo "Wiki-Format: zim 0.6" >> "${filename}".md
 echo -e "===== $(date +"Draft page %Y-%m-%d") =====" >> "${filename}".md
 echo -e "Created $(date +"[[Zettelkasten:%Y:%m:%d|%Y-%m-%d]]")" >> "${filename}".md
-echo -e "[*] ** $(date +"Draft page %Y-%m-%d") ** [[Artelier:Input|Input]] \n\n" >> "${filename}".md
-mv ~/Downloads/"${filename}".md ~/Alte-Inputs/"${filename}".md
-mv "${filename}".md ~/Downloads/"${filename}".md
+echo -e "[*] ** $(date +"Draft page %Y-%m-%d") ** [[Artelier:Inputs|Inputs]] \n\n" >> "${filename}".md
+oldfilename=$(date -d "yesterday 13:00" +"Draft_page_%Y-%m-%d")
+mv -f ~/Downloads/"${oldfilename}".md ~/Alte-Inputs/"${oldfilename}".md 2>/dev/null
+mv -f ~/Downloads/"${filename}".md ~/Alte-Inputs/"${filename}".md 2>/dev/null
+mv -f "${filename}".md ~/Downloads/"${filename}".md 2>/dev/null
