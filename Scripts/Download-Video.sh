99fde1591916944131d3190e06495d64d75bfa7f
christiang7 <98482045+christiang7@users.noreply.github.com>
2025-02-17 18:13:13 +0100

    update Download-Video.sh

--- Scripts/Download-Video.sh
+++ Scripts/Download-Video.sh
@@ -38,7 +38,7 @@ echo $format
 
 yt-dlp --no-mtime --write-thumbnail --fixup force -f "$format" --add-chapters --sub-langs "en,de" --write-sub --write-auto-sub --sub-format "vtt" --external-downloader aria2c --http-chunk-size 5M --downloader-args aria2c:"-c -j 2 -s 2 -x 4 -k 50M" -o "$outputDir/%(title)s.%(ext)s" -i "${website}"
 
-ofile=$(yt-dlp -f "[ext=mp4]" --print filename -s "${website}" -o '%(title)s.%(ext)s')
+ofile=$(yt-dlp --print filename -s "${website}" -o '%(title)s').mp4
 extens=${ofile##*.}
 name=${ofile%.*}
 additiontext="$(yt-dlp --get-description ${website})"
--- Scripts/run-cell.sh
+++ Scripts/run-cell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Mo 17. Feb 17:40:06 CET 2025
+# Mo 17. Feb 18:06:07 CET 2025
 export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
-noweb.py -Rtabs2topic.sh tabs2topic.sh.md > tabs2topic.sh && echo 'tabs2topic.sh' && date
+noweb.py -RDownload-Video.sh Download-Video.md > Download-Video.sh && echo "Download-Video.sh" && date
